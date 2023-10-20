26d97c5653af728d9f46b380df98b207d2e151a2
GEDJr <gelimvje@hotmail.com>
2023-10-21 07:35:12 +0800

    Added Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// https://tailwindcss.com/blog/standalone-cli
+// https://www.matsimitsu.com/blog/2022-01-04-taliwind-cli-with-yew-and-trunk
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // './src/**/*.rs',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
+}
+
